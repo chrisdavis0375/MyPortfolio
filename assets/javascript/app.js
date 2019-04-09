a960201fb20de0309695f1d5b40e63d812789793
Bruce Grayson <chrisdavis0375@gmail.com>
2019-04-09 02:31:28 -0700

    Introduction box completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
     console.log(" Javascript is working.");
+
+
+    $("#portfoliobtn").on('click', function() {
+        console.log("Portfolio button has been clicked.")
+        //var introBox = $("#introBox");
+
+        $("#introBox").hide();
+        $("#aboutMeBox").style.display = "show";
+    })
 });
