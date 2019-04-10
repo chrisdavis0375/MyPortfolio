2c5b9056e4a78d032dd8af2116f88aeb5ea7d0b8
Bruce Grayson <chrisdavis0375@gmail.com>
2019-04-10 15:46:59 -0700

    Added other content pages and background photos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,4 +10,40 @@ $(document).ready(function () {
         document.getElementById("nav").style.display = "block";
         document.getElementById("mainContent").style.display = "block";
     })
+
+    $("#aboutbtn").on('click', function() {
+        console.log("About button clicked.");
+
+        document.getElementById("aboutMeBox").style.display = "block";
+        $("#contactBox").hide();
+        $("#projectBox").hide();
+        $("#resumeBox").hide();
+    });
+
+    $("#workbtn").on('click', function() {
+        console.log("Work button clicked.")
+
+        document.getElementById("projectBox").style.display = "block";
+        $("#contactBox").hide();
+        $("#aboutMeBox").hide();
+        $("#resumeBox").hide();
+    });
+
+    $("#contactbtn").on('click', function() {
+        console.log("Contact button clicked.")
+
+        document.getElementById("contactBox").style.display = "block";
+        $("#aboutMeBox").hide();
+        $("#projectBox").hide();
+        $("#resumeBox").hide();
+    });
+
+    $("#resumebtn").on('click', function() {
+        console.log("Resume button clicked.")
+
+        document.getElementById("resumeBox").style.display = "block";
+        $("#contactBox").hide();
+        $("#projectBox").hide();
+        $("#aboutMeBox").hide();
+    });
 });
